96c2b6e1ea19d825a73db47b1fa61a1cf66147a0
Brenda Cagua <bg.0115@hotmail.com>
2023-08-01 21:32:51 -0500

    añadido datos

---
+++ app.js
@@ -0,0 +1,9 @@
+let divDuolingo = document.getElementById ("Duolingo")
+
+let divLyrics = document.getElementById ("Lyrics-Training")
+
+let divBusuu = document.getElementById ("Busuu")
+
+let divMemrise = document.getElementById ("Memrise")
+
+let divLingualia = document.getElementById ("Lingualia")
